75c9957865e3c608fdf520d3076b552248bd3cb0
Emile Taverne <emile_taverne@hotmail.com>
2015-01-14 22:42:20 +0100

    Ajout des factory

---
+++ LoginServer/Controller/UserController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using LoginServer.Model;
+using LoginServer.Manager;
+
+namespace LoginServer.Controller
+{
+    public class UserController : IController
+    {
+        /// <summary>
+        /// Redirige la requête vers l'action correspondante
+        /// </summary>
+        /// <param name="stream">Flux de données à traiter</param>
+        public Response parser(Stream stream)
+        {
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                try
+                {
+                    ushort idAction = reader.ReadUInt16();
+
+                    switch (idAction)
+                    {
+                        case 1:
+                            this.loginAction(
+                                new string(reader.ReadChars(User.LOGIN_LENGTH)),
+                                new string(reader.ReadChars(User.PASSWORD_LENGTH))
+                            );
+                            break;
+                        case 2:
+                            this.loginAction(
+                                new string(reader.ReadChars(User.LOGIN_LENGTH)),
+                                new string(reader.ReadChars(User.PASSWORD_LENGTH))
+                            );
+                            break;
+                        default:
+                            Logger.log(typeof(UserManager), "L'action n'existe pas : " + idAction, Logger.LogType.Error);
+                            //Todo reponse erreur
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.log(typeof(UserController), e.Message, Logger.LogType.Error);
+                }
+            }
+            return (Response)new object();
+        }
+
+        /// <summary>
+        /// Essai de connecter l'utilisateur
+        /// </summary>
+        private void loginAction(string login, string password)
+        {
+            User user = null;
+
+            try
+            {
+                user = ManagerFactory.getUserManager().getUser(login, password);
+            }
+            catch (Exception e)
+            {
+                Logger.log(typeof(UserManager), "Impossible de connecter l'utilisateur : " + e.Message, Logger.LogType.Error);
+            }
+
+            if (user == null)
+            {
+                //Todo construire réponse erreur
+            }
+            else
+            {
+                //Todo inscription dans un serveur libre
+                //Todo construire réponse succés
+            }
+        }
+
+        /// <summary>
+        /// Enregistre un nouvel utilisateur
+        /// </summary>
+        private void registerAction(string login, string password)
+        {
+            try
+            {
+                ManagerFactory.getUserManager().registration(login, password);
+            }
+            catch (Exception e)
+            {
+                Logger.log(typeof(UserManager), "Impossible d'enregistrer l'utilisateur : " + e.Message, Logger.LogType.Error);
+                //Todo construire réponse erreur
+                return;
+            }
+
+            //Todo construire réponse succés
+        }
+    }
+}
---
+++ LoginServer/Manager/ManagerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LoginServer.Manager
+{
+    public static class ManagerFactory
+    {
+        private static UserManager userManager;
+
+        /// <summary>
+        /// Récupère le manager des utilisateurs
+        /// </summary>
+        /// <returns></returns>
+        public static UserManager getUserManager()
+        {
+            if (ManagerFactory.userManager == null)
+            {
+                ManagerFactory.userManager = new UserManager();
+            }
+            return ManagerFactory.userManager;
+        }
+    }
+}
